9cf8215480d788fbcc64474f69b4eea5b3c7a6aa
Sergio Darinel Mendoza Merida <darinelmendoza277@gmail.com>
2022-02-14 10:30:18 -0600

    Puntos Extra

---
+++ README.md
@@ -0,0 +1,25 @@
+FUNCIONAMIENTO DEL PROGRAMA
+
+Este programa será utilizado para calcular el pago a realizar de una persona que adquiera un seguro.
+Para calcular el pago total se concideran los siguientes aspectos: la edad del asegurado, la edad de su
+conyugue (si estuviese casado), la cantidad de hijos que tenga (si los tuviese), la cantidad de propiedades 
+que tiene a su nombre y el salario mensual que recibe. El programa funciona recopilando los datos antes mencionados
+y aplicando un recargo por cada uno de ellos. finalmente muestra en pantalla el recargo que se aplicara y 
+el total a pagar.
+
+PARTES DEL PROBLEMA A CONSIDERAR
+
+Las partes del problema a considerar son: 
+- De que manera vamos a obtener los datos del usuario
+- Las variables para la obtención de los datos
+- El recargo que se aplicará por cada aspecto considerado en el monto del seguro
+- La estructura condicional que utilizaremos para el proceso de lógica
+- Las operaciones matematicas para el calculo del precio final
+- La forma en la que el usuario pueda continuar o cerra el programa
+- Mostrar los datos al usuario.
+
+MEJORAS QUE SE PODRÍAN REALIZAR
+
+Para mejorar el programa, principalmente se debería crear una interfáz mas amigable con el usuario y quizá también crear 
+un algoritmo que en base a las propiedades a su nombre y el salario del asegurado determine si puede costear
+los gastos del seguro.
--- proyecto.js
+++ proyecto.js
@@ -12,94 +12,121 @@ var casado_25 = 0.2 // 20%
 var casado_50 = 0.3 // 30%
 
 var hijos_recargo = 0.2 // 20%
+var propiedades_recargo = 0.35
 
+var propiedades = 0
+var cantidadPropiedades = 0
 
+var salario = 0
+var cantidad_salario = 0
+var salario_recargo = 0.05
+var continuar = 0 //para continuar con cotizaciones
 //Recargo
 var recargo = 0
 var recargo_total = 0
 
 //Precio final 
 var precio_final = 0
+while (continuar !== "SALIR"){
+	//Mensajes de alerta para ingresar datos 
+	var nombre = prompt("Ingrese su nombre, por favor")
+	var edad = prompt("¿Cuantos años tiene? Ingrese solamente números ")
+	
+	var casado = prompt("¿Está casado actualmente?")
+	//Comprobamos la edad del cónyuge, solamente si se está casado/a
+	var edad_conyuge
+	if("SI" == casado.toUpperCase()){
+  	edad_conyuge = prompt("¿Que edad tiene su esposo/a?", "si/no")
+	}
+	//convirtiendo las edades ingresadas a números 
+	var edad_numero = parseInt(edad)
+	var edad_conyuge_numero = 0
+	//convirtiendo la edad del cónyuge si se esta casado/a
+	if("SI" == casado.toUpperCase()){
+	  edad_conyuge_numero = parseInt(edad_conyuge)
+	}
+	
+	var hijos = prompt("¿Tiene hijos o hijas?")
+	var tien_hijos = false
+	//Comprobamos la cantidad de hijos solamente si los tienen
+	if ("SI" == hijos.toUpperCase()) {
+		tien_hijos = true
+		var cantidad_hijos = prompt("Cuantos hijos tiene")
+		cantidad_hijos = parseInt(cantidad_hijos)
+	}
+	propiedades = prompt("¿Tiene propiedades a su nombre")
+	if ("SI" == propiedades.toUpperCase()) {
+		cantidadPropiedades = prompt("Cuántas propiedades tiene a su nombre")
+		cantidadPropiedades = parseInt(cantidadPropiedades)
+		recargo = cantidadPropiedades * precio_base * propiedades_recargo
+		recargo_total = recargo_total + recargo
+	}
+	salario = prompt("Cuenta con un salario fijo?")
+	if ("SI" == salario.toUpperCase()) {
+		cantidad_salario = prompt("De cuanto es su ingreso salarial?")
+		cantidad_salario = parseInt(cantidad_salario)
+		recargo = cantidad_salario * salario_recargo
+		recargo_total = recargo_total + recargo
+	}
+	/**
+	 * 1. convierta la cantidad de hijos a numero
+	 */
 
-//Mensajes de alerta para ingresar datos 
-var nombre = prompt("Ingrese su nombre, por favor")
-var edad = prompt("¿Cuantos años tiene? Ingrese solamente números ")
-
-var casado = prompt("¿Está casado actualmente?")
-//Comprobamos la edad del cónyuge, solamente si se está casado/a
-var edad_conyuge
-if("SI" == casado.toUpperCase()){
-  edad_conyuge = prompt("¿Que edad tiene su esposo/a?", "si/no")
-}
-//convirtiendo las edades ingresadas a números 
-var edad_numero = parseInt(edad)
-var edad_conyuge_numero = 0
-//convirtiendo la edad del cónyuge si se esta casado/a
-if("SI" == casado.toUpperCase()){
-  edad_conyuge_numero = parseInt(edad_conyuge)
-}
-
-var hijos = prompt("¿Tiene hijos o hijas?")
-var tien_hijos = false
-//Comprobamos la cantidad de hijos solamente si los tienen
-if ("SI" == hijos.toUpperCase()) {
-	tien_hijos = true
-	var cantidad_hijos = prompt("Cuantos hijos tiene")
-	cantidad_hijos = parseInt(cantidad_hijos)
-}
-
-/**
- * 1. convierta la cantidad de hijos a numero
- */
-
-//Aquí debe calcular el recargo total basado en las respuestas ingresadas
-
-//Aquí es donde debe de calcular los recargos y el valor final
-//Ejemplo (Debe completar los condicionales): Recargo por edad del asegurado 
-if(edad_numero>=18 && edad_numero<25){
-  //Calculamos el recargo en base a la edad 
-  recargo = precio_base * edad_18
-  //Sumamos todos los recargos que hemos obtenido
-  recargo_total = recargo_total + recargo
-}
-else if(edad_numero>=25 && edad_numero<=49){
-	recargo = precio_base * edad_25
-	recargo_total = recargo_total + recargo
-}
-else if (edad_numero>49) {
-	recargo = precio_base * edad_50
-	recargo_total = recargo_total + recargo
-}
- console.log(recargo)
+	//Aquí debe calcular el recargo total basado en las respuestas ingresadas
+	
+	//Aquí es donde debe de calcular los recargos y el valor final
+	//Ejemplo (Debe completar los condicionales): Recargo por edad del asegurado 
+	if(edad_numero>=18 && edad_numero<25){
+	  //Calculamos el recargo en base a la edad 
+	  recargo = precio_base * edad_18
+	  //Sumamos todos los recargos que hemos obtenido
+	  recargo_total = recargo_total + recargo
+	}
+	else if(edad_numero>=25 && edad_numero<=49){
+		recargo = precio_base * edad_25
+		recargo_total = recargo_total + recargo
+	}
+	else if (edad_numero>49) {
+		recargo = precio_base * edad_50
+		recargo_total = recargo_total + recargo
+	}
+	 console.log(recargo)
 //aqui puede colocar un else if() con el siguiente rango
 
 /** 
  * 2. Recargo por la edad del conyuge
  */
- if(edad_conyuge_numero>=18 && edad_conyuge_numero<=24){
- 	recargo = precio_base * casado_18
- 	recargo_total = recargo_total + recargo
- }
- else if (edad_conyuge_numero>=25 && edad_conyuge_numero<=49) {
- 	recargo = precio_base * casado_25
- 	recargo_total = recargo_total + recargo
- }
- else if (edad_conyuge_numero>49) {
- 	recargo = precio_base * casado_50
- 	recargo_total = recargo_total + recargo
- }
- console.log(recargo)
+	 if(edad_conyuge_numero>=18 && edad_conyuge_numero<=24){
+	 	recargo = precio_base * casado_18
+	 	recargo_total = recargo_total + recargo
+	 }
+	 else if (edad_conyuge_numero>=25 && edad_conyuge_numero<=49) {
+	 	recargo = precio_base * casado_25
+	 	recargo_total = recargo_total + recargo
+	 }
+	 else if (edad_conyuge_numero>49) {
+	 	recargo = precio_base * casado_50
+	 	recargo_total = recargo_total + recargo
+	 }
+	 console.log(recargo)
 /**
  * 3. Recargo por la cantidad de hijos 
  */ 
-if (tien_hijos == true) {
-	recargo = (precio_base * hijos_recargo) * cantidad_hijos
-	recargo_total = recargo_total + recargo
-	console.log(recargo)
-}
-precio_final = precio_base + recargo_total
+	if (tien_hijos == true) {
+		recargo = (precio_base * hijos_recargo) * cantidad_hijos
+		recargo_total = recargo_total + recargo
+		console.log(recargo)
+	}
+	precio_final = precio_base + recargo_total
 //Resultado
-alert ("Para el asegurado "+nombre)
-alert ("El recargo total sera de: "+recargo_total)
-alert ("El precio sera de: "+precio_final)
+	alert ("Para el asegurado "+nombre)
+	alert ("El recargo total sera de: "+recargo_total)
+	alert ("El precio sera de: "+precio_final)
+
+	continuar = prompt("Si no necesita realizar otra cotización escriba Salir de lo contrario presione aceptar")
+	continuar = continuar.toUpperCase()
+	recargo = 0
+	recargo_total = 0
+	precio_final = 0
+}
 
